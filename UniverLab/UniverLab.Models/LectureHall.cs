28a22ace4b23e5800cf253af11236fe91e9629c5
Azat <46015824+serikovskii@users.noreply.github.com>
2019-04-08 11:23:29 +0600

    создание сущностей

---
+++ UniverLab/UniverLab.Models/LectureHall.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniverLab.Models
+{
+    public class LectureHall
+    {
+        public int Id { get; set; }
+        public int Capavity { get; set; }
+    }
+}
---
+++ UniverLab/UniverLab.Models/Schedule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniverLab.Models
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+        public int SubjectId { get; set; }
+        public int GroupId { get; set; }
+        public int ProfessorId { get; set; }
+        public int LectureHallId { get; set; }
+        public DateTime Time { get; set; }
+        public DayOfWeek Day { get; set; }
+    }
+}
